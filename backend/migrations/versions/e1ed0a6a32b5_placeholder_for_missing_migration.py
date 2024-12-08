0907113d867e27ef4c4fc67a72e490eb8264d3ef
Saeed <yasrebi.saeed@gmail.com>
2024-12-08 18:41:40 +0000

    Making some changes on the kustomize and docker related files and migrations

---
+++ backend/migrations/versions/e1ed0a6a32b5_placeholder_for_missing_migration.py
@@ -0,0 +1,24 @@
+"""Placeholder for missing migration
+
+Revision ID: e1ed0a6a32b5
+Revises: e67aea7e7a6e
+Create Date: 2024-12-09 12:15:54.375229
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1ed0a6a32b5'
+down_revision = 'e67aea7e7a6e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
